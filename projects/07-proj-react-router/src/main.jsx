28d06c2356a3df5402ec053d2c0158c7c6f071db
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-07 07:39:40 +0000

    adjust main file

--- projects/07-proj-react-router/src/main.jsx
+++ projects/07-proj-react-router/src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import { About, Contact, Home } from './components/index.js'
+// ###### Old Syntax to create Router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
+
+// ########## New Syntax to create router 
+// const router = createBrowserRouter([
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout />}>
+//       <Route path='' element={<Home />} />
+//       <Route path='about' element={<About />} />
+//       <Route path='contact' element={<Contact />}/>
+//     </Route>
+//   )
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
