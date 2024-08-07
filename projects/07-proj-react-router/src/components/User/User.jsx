ee303e0ea33e17a3208c0f1f80a72dc1b800d4a6
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-07 08:08:47 +0000

    create user component

--- projects/07-proj-react-router/src/components/User/User.jsx
+++ projects/07-proj-react-router/src/components/User/User.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function User() {
+  const {id} = useParams()
   return (
-    <div>User: </div>
+    <div className='bg-gray-700 p-4 text-white text-3xl align-middle text-center'>User: {id}</div>
   )
 }
 
