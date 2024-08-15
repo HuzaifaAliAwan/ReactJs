138ca03200225246c6a2e0bd674454badf4d703b
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-15 05:49:26 +0000

    make changes in Login

--- projects/09-proj-mini-context/src/components/Login.jsx
+++ projects/09-proj-mini-context/src/components/Login.jsx
@@ -2,13 +2,33 @@ import React, { useState, useContext } from "react";
 import UserContext from "../context/UserContext";
 
 function Login() {
-  const handleSubmit = () => {};
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(UserContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
 
   return (
     <div>
       <h2>Login</h2>
-      <input type="text" placeholder="username" />
-      <input type="text" placeholder="password" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="password"
+        value={password}
+        onClick={(e) => setPassword(e.target.value)}
+      />
+      <br />
       <button onClick={handleSubmit}>Submit</button>
     </div>
   );
