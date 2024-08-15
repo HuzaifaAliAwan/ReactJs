73c51237598fc9327c75aa2708b158fe8a0f282a
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-15 05:49:59 +0000

    Insert components

--- projects/09-proj-mini-context/src/App.jsx
+++ projects/09-proj-mini-context/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
     <UserContextProvider>
-      <h1>Hello world</h1>
+      <h1>ReactJs</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   );
 }
