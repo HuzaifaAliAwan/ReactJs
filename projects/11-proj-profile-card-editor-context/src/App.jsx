dfe2fe66dfdfacb401828a2fffbe0e4c7a9285c2
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-16 07:44:11 +0000

    use components in App

--- projects/11-proj-profile-card-editor-context/src/App.jsx
+++ projects/11-proj-profile-card-editor-context/src/App.jsx
@@ -1,5 +1,19 @@
+import ProfileCard from "./components/profileCard";
+import ProfileEditCard from "./components/profileEditCard";
+
+import { ProfileContextProvider } from "./context/profileContext/profileContextProvider";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <div className="w-full h-screen flex flex-col md:flex-row bg-gray-100 p-4 md:p-16 justify-between">
+        <ProfileContextProvider>
+          <ProfileEditCard />
+          <ProfileCard />
+        </ProfileContextProvider>
+      </div>
+    </>
+  );
 }
 
 export default App;
