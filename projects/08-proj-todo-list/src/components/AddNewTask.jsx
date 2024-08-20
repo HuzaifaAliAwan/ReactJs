178a4c7c2b17fde01e1e9854e04e78c5c557e08f
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-20 12:52:12 +0000

    add new task template

--- projects/08-proj-todo-list/src/components/AddNewTask.jsx
+++ projects/08-proj-todo-list/src/components/AddNewTask.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 function AddNewTask() {
-  return <div>AddNewTask</div>;
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex flex-row justify-center">
+        <h1 className="font-inter font-bold text-[24px]">Add Task</h1>
+      </div>
+      <div>
+        <h2 className="font-inter font-medium text-[16px]">Task Name</h2>
+        <input
+          className="p-3 bg-[#F7F8FC] w-full rounded-lg mt-2 outline-none hover:bg-[#e7e8eb] focus:bg-[#e7e8eb] focus:outline-none transition-all"
+          placeholder="Enter Task Name"
+          type="text"
+          name="Task Name"
+          id="taskname"
+        />
+      </div>
+      <div>
+        <h2 className="font-inter font-medium text-[16px]">Day</h2>
+        <input
+          className="p-3 bg-[#F7F8FC] w-full rounded-lg mt-2 outline-none hover:bg-[#e7e8eb] focus:bg-[#e7e8eb] focus:outline-none transition-all"
+          placeholder="Select Day"
+          type="text"
+          name="Task Date"
+          id="taskdate"
+        />
+      </div>
+      <div>
+        <h2 className="font-inter font-medium text-[16px]">Description</h2>
+        <input
+          className="p-3 bg-[#F7F8FC] w-full rounded-lg mt-2 outline-none hover:bg-[#e7e8eb] focus:bg-[#e7e8eb] focus:outline-none transition-all"
+          placeholder="Enter Description"
+          type="text"
+          name="Task Description"
+          id="taskdescription"
+        />
+      </div>
+
+      <button
+        className="w-full mt-3 p-3 bg-[#E2EBFA] rounded-lg text-[#1563FB] font-medium hover:bg-[#9ab8ec] hover:text-[#ffffff] transition-all"
+        type="button"
+      >
+        Create
+      </button>
+    </div>
+  );
 }
 
 export default AddNewTask;
