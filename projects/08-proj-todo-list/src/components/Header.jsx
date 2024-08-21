6f680677ce2e274b5cfa4364dd7f2aa375eb56fb
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-21 07:13:19 +0000

    correct path, add realtime date, change some tags

--- projects/08-proj-todo-list/src/components/Header.jsx
+++ projects/08-proj-todo-list/src/components/Header.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { AddNewTask } from "./";
 import { NavLink } from "react-router-dom";
 
 function Header() {
+  const [addTaskShow, setAddTaskShow] = useState(false);
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    setDate(new Date());
+  }, []);
   return (
     <>
       {/* First row */}
@@ -10,21 +15,26 @@ function Header() {
         <div>
           <h1 className="font-inter font-bold text-[24px]">Tasks</h1>
           <h2 className="font-inter font-normal text-[16px] text-[#969696]">
-            Friday, 9 August
+            {date.toLocaleDateString("en-US", { weekday: "long" })}, &nbsp;
+            {date.toLocaleDateString("en-US", { day: "numeric" })}
+            &nbsp;
+            {date.toLocaleDateString("en-US", { month: "long" })}
           </h2>
         </div>
         <div className="flex flex-col justify-center">
-          <NavLink
-            to={"/add"}
-            className="py-3 px-5 bg-[#E2EBFA] rounded-lg text-[#1563FB] text-[16px] hover:bg-[#9ab8ec] hover:text-[#ffffff] transition-all font-semibold"
+          <div
+            onClick={() => {
+              setAddTaskShow(true);
+            }}
+            className="py-3 px-5 bg-[#E2EBFA] rounded-lg text-[#1563FB] text-[16px] hover:bg-[#9ab8ec] hover:text-[#ffffff] transition-all font-semibold cursor-pointer"
           >
             + New Task
-          </NavLink>
+          </div>
         </div>
       </div>
       {/* Second row */}
       <div className="mt-5 flex flex-row justify-start">
-        <NavLink to={"/all"}>
+        <NavLink to={"/"}>
           {({ isActive }) => (
             <div
               className={`${
@@ -62,6 +72,17 @@ function Header() {
             </div>
           )}
         </NavLink>
+      </div>
+
+      <div
+        className={`${
+          addTaskShow ? "block" : "hidden"
+        } fixed top-0 left-0 w-full h-full bg-[#d9d9d9] bg-opacity-20 backdrop-blur-sm z-10 drop-shadow-lg`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="bg-white p-5 mx-5 mb-5 mt-[25%] rounded-lg ">
+          <AddNewTask setAddTaskShow={setAddTaskShow} />
+        </div>
       </div>
     </>
   );
