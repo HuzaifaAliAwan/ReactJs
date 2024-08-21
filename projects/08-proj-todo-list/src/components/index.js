4bde72c7164c9d53226b78192904231e0e4ff6e3
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-21 07:13:56 +0000

    export component

--- projects/08-proj-todo-list/src/components/index.js
+++ projects/08-proj-todo-list/src/components/index.js
@@ -2,5 +2,6 @@ import Header from "./Header";
 import AllTasks from "./AllTasks";
 import CompletedTasks from "./CompletedTasks";
 import AddNewTask from "./AddNewTask";
+import TaskCard from "./TaskCard";
 
-export { Header, AllTasks, CompletedTasks, AddNewTask };
+export { Header, AllTasks, CompletedTasks, AddNewTask, TaskCard };
