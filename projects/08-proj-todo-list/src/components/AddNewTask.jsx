fa0bf52b542c42afe4b1d9c56d115cfc36b9fc73
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2024-08-21 07:12:24 +0000

    make some changes

--- projects/08-proj-todo-list/src/components/AddNewTask.jsx
+++ projects/08-proj-todo-list/src/components/AddNewTask.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-function AddNewTask() {
+function AddNewTask({ setAddTaskShow }) {
   return (
     <div className="flex flex-col gap-3">
       <div className="flex flex-row justify-center">
-        <h1 className="font-inter font-bold text-[24px]">Add Task</h1>
+        <h1 className="font-inter font-bold text-[24px] w-full align-middle text-center">
+          Add Task
+        </h1>
+        <button
+          onClick={() => {
+            setAddTaskShow(false);
+          }}
+          className="px-3 py-1 bg-[#E2EBFA] rounded-lg text-[#1563FB] text-[16px] hover:bg-[#9ab8ec] hover:text-[#ffffff] font-semibold text-lg"
+        >
+          &times;
+        </button>
       </div>
       <div>
         <h2 className="font-inter font-medium text-[16px]">Task Name</h2>
         <input
-          className="p-3 bg-[#F7F8FC] w-full rounded-lg mt-2 outline-none hover:bg-[#e7e8eb] focus:bg-[#e7e8eb] focus:outline-none transition-all"
+          className="p-3 bg-[#F7F8FC] w-full rounded-lg mt-2 outline-none hover:bg-[#e7e8eb] focus:bg-[#e7e8eb] focus:outline-none  "
           placeholder="Enter Task Name"
           type="text"
           name="Task Name"
@@ -18,18 +28,21 @@ function AddNewTask() {
       </div>
       <div>
         <h2 className="font-inter font-medium text-[16px]">Day</h2>
-        <input
-          className="p-3 bg-[#F7F8FC] w-full rounded-lg mt-2 outline-none hover:bg-[#e7e8eb] focus:bg-[#e7e8eb] focus:outline-none transition-all"
-          placeholder="Select Day"
-          type="text"
-          name="Task Date"
-          id="taskdate"
-        />
+        <select className="w-full py-3 px-5 mt-2 bg-[#E2EBFA] rounded-lg text-[#1563FB] text-[16px] hover:bg-[#9ab8ec] hover:text-[#ffffff] font-semibold cursor-pointer outline-none">
+          <option value="">Select Day</option>
+          <option value="Monday">Monday</option>
+          <option value="Tuesday">Tuesday</option>
+          <option value="Wednesday">Wednesday</option>
+          <option value="Thursday">Thursday</option>
+          <option value="Friday">Friday</option>
+          <option value="Saturday">Saturday</option>
+          <option value="Sunday">Sunday</option>
+        </select>
       </div>
       <div>
         <h2 className="font-inter font-medium text-[16px]">Description</h2>
         <input
-          className="p-3 bg-[#F7F8FC] w-full rounded-lg mt-2 outline-none hover:bg-[#e7e8eb] focus:bg-[#e7e8eb] focus:outline-none transition-all"
+          className="p-3 bg-[#F7F8FC] w-full rounded-lg mt-2 outline-none hover:bg-[#e7e8eb] focus:bg-[#e7e8eb] focus:outline-none  "
           placeholder="Enter Description"
           type="text"
           name="Task Description"
@@ -38,7 +51,7 @@ function AddNewTask() {
       </div>
 
       <button
-        className="w-full mt-3 p-3 bg-[#E2EBFA] rounded-lg text-[#1563FB] font-medium hover:bg-[#9ab8ec] hover:text-[#ffffff] transition-all"
+        className="w-full mt-3 p-3 bg-[#E2EBFA] rounded-lg text-[#1563FB] font-medium hover:bg-[#9ab8ec] hover:text-[#ffffff]  "
         type="button"
       >
         Create
